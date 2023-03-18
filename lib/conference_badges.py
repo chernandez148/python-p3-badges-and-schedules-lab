4ef3952e07ae27e181e28c05cc99070a46f78a51
Christian Hernandez <chernandez148@DESKTOP-3F6UKNT.localdomain>
2023-03-18 13:48:19 -0700

    updated

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,52 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    return [f'Hello, my name is {name}.' for name in names]
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, 9)
+    
+    assignments = []
+    for room in rooms:
+        assignments.append(f'Hello, {names[room - 1]}! You\'ll be assigned to room {room}!')
+    
+    return assignments
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    
+    for assignment in assign_rooms(names):
+        print(assignment)
+
+
+# def badge_maker(name):
+#     return f'Hello, my name is {name}.'
+# badge_maker("Guido van Rossum")
+
+# def batch_badge_creator(names):
+#     greet = []
+
+#     for eachName in names:
+#         greet.append(f"Hello, my name is {eachName}.")
+#     return greet
+# batch_badge_creator(['Guido', 'Edsger', 'Ada', "Grace"])
+
+# def assign_rooms(names):
+#     room_num = 1
+#     assign_rooms = []
+
+#     while room_num < len(names):
+#         for eachName in names:
+#             assign_rooms.append(f"Hello, {eachName}! You'll be assigned to room {room_num}!")
+#             room_num += 1
+#     return assign_rooms
+# assign_rooms(['Guido', 'Edsger', 'Ada', 'Grace'])
+
+# def printer(names):
+#     return batch_badge_creator(names)
+#     return assign_rooms(names)
+# printer(["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+
+
